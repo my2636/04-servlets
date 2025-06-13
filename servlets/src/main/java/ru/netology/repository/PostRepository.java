f5d2f66976fef8689124b25f2be9c5151f8bf138
myakk <myakk015@gmail.com>
2025-06-13 10:45:12 +0300

    commit

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,62 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-// Stub
+/*
+ Задача
+Осуществите рефакторинг кода.
+Реализуйте репозиторий с учётом того, что методы репозитория могут вызываться конкурентно, т. е. в разных потоках.
+Как должен работать save:
+
+Если от клиента приходит пост с id=0, значит, это создание нового поста. Вы сохраняете его в списке и присваиваете ему
+новый id. Достаточно хранить счётчик с целым числом и увеличивать на 1 при создании каждого нового поста.
+
+Если от клиента приходит пост с id !=0, значит, это сохранение (обновление) существующего поста. Вы ищете его в списке
+по id и обновляете. Продумайте самостоятельно, что вы будете делать, если поста с таким id не оказалось: здесь могут быть
+разные стратегии.
+*/
+
 public class PostRepository {
+  private int postCount = 0;
+  List<Post> postList = new CopyOnWriteArrayList();
+
   public List<Post> all() {
-    return Collections.emptyList();
+    return postList;
   }
 
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return postList.stream().findFirst();
   }
 
   public Post save(Post post) {
-    return post;
+    try {
+      Optional<Post> optionalPost = getById(post.getId());
+      if (optionalPost.isPresent()) {
+        Post post1 = optionalPost.get();
+        post1.setContent(post.getContent());
+        return post1;
+      } else {
+        postCount++;
+        post.setId(postCount);
+        postList.add(post);
+        return post;
+      }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return null;
+    }
   }
 
   public void removeById(long id) {
+    try {
+      postList.remove(id);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
   }
 }
--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -7,43 +7,33 @@ import ru.netology.service.PostService;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MainServlet extends HttpServlet {
   private PostController controller;
 
   @Override
-  public void init() {
+  final public void init() {
     final var repository = new PostRepository();
     final var service = new PostService(repository);
     controller = new PostController(service);
   }
 
   @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+  final protected void service(HttpServletRequest req, HttpServletResponse resp) {
     // если деплоились в root context, то достаточно этого
     try {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
       // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
-        return;
+      if (method.equals("GET")) {
+        doGet(path, resp);
       }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.getById(id, resp);
-        return;
+      if (method.equals("POST")) {
+        doPost(path, req, resp);
       }
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.removeById(id, resp);
-        return;
+      if (method.equals("DELETE")) {
+        doDelete(path, resp);
       }
       resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     } catch (Exception e) {
@@ -51,5 +41,31 @@ public class MainServlet extends HttpServlet {
       resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
   }
+
+    final protected void doGet(String path, HttpServletResponse resp) throws IOException {
+        if (path.equals("/api/posts")) {
+            controller.all(resp);
+            return;
+        }
+        if (path.matches("/api/posts/\\d+")) {
+            // easy way
+            final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+            controller.getById(id, resp);
+        }
+    }
+
+    final protected void doPost(String path, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (path.equals("/api/posts")) {
+            controller.save(req.getReader(), resp);
+        }
+    }
+
+    final protected void doDelete(String path, HttpServletResponse resp) {
+        if (path.matches("/api/posts/\\d+")) {
+            // easy way
+            final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+            controller.removeById(id, resp);
+        }
+    }
 }
 
